55ee76ffd729609438402ff6c9e94fbe9ed2aabc
mercuryfrost <chris.cassie@gmail.com>
2025-07-19 18:10:34 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const recipeRoutes = require("./routes/recipe");
+const recipeRoutes = require("./routes/name");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
